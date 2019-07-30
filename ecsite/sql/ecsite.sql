08468db289c1c7fd7db6b77476040168eefe3a9f
k-ritsuk <rikbaske@gmail.com>
2019-07-30 16:14:48 +0900

    7/30

--- ecsite/sql/ecsite.sql
+++ ecsite/sql/ecsite.sql
@@ -17,9 +17,7 @@ user_name varchar(50),
 
 insert_date datetime;
 updated_date datetime
-
 );
-
 
 drop table if exists item_info_transaction;
 
@@ -33,9 +31,7 @@ item_stock int,
 
 insert_date datetime;
 updated_date datetime
-
 );
-
 
 drop table if exists user_buy_item_transaction;
 
@@ -48,11 +44,10 @@ total_price int,
 total_count int,
 user_master_id varher(16),
 pay varchar(30),
+
 insert_date datetime;
 delete_date datetime
-
 );
-
 
 INSERT INTO item_info_transaction(item_name,item_price,item_stock) VALUES("ÉmÅ[ÉgBook",100,50);
 INSERT INTO login_user_transaction(login_id,login_pass,user_name) VALUES("internous","internous01","test");
